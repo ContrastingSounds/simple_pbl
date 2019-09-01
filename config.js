813657b366509e9fb311bdc63aa29c63e7421339
Jon Walls <jonathan.walls@looker.com>
2019-09-01 16:01:43 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,54 @@
+var lookerTheme = 'Looker'
+
+var globalConfig = {
+  headerText: 'Introduction to Looker',
+  title: 'Looker Proof of Value',
+  logo: 'https://storage.googleapis.com/media-jw-test-environment/images/Vodafone%20iView%20Logo.png',
+  logoText: 'OSR',
+
+  navbarMenu: [
+    {
+      text: 'Business Value',
+      url: 'business_value.html',
+    },
+    {
+      text: 'Time to Market',
+      url: 'time_to_market.html',
+    },
+    {
+      text: 'Radical Simplicity',
+      url: 'cloud_native_architecture.html',
+    },
+  ],
+  
+  sidebarImage: 'https://public-vodafone-a.s3-eu-west-1.amazonaws.com/wp-content/uploads/2019/07/unlimited-1024x569.jpg',
+  sidebarItems: [
+    {
+      type: 'li',
+      icon: 'dashboard',
+      text: 'Product Overview',
+    },
+    {
+      type: 'li',
+      icon: 'bar_chart',
+      text: 'Account Overview',
+    },
+    {
+      type: 'subheader',
+      text: 'Replicated Dashboards',
+    },
+    {
+      type: 'li',
+      icon: 'bubble_chart',
+      text: 'Sales Performance',
+    },
+  ],
+
+  dashboards: {
+    'Sales Performance': 'https://saleseng.dev.looker.com/embed/dashboards/321?hide_title=true&theme=' + lookerTheme,
+    'Product Overview': 'https://saleseng.dev.looker.com/embed/dashboards/522?hide_title=true&theme=' + lookerTheme,
+    'Account Overview': 'https://saleseng.dev.looker.com/embed/dashboards/312?hide_title=true&theme=' + lookerTheme,
+  },
+
+  mainDashboard: 'https://saleseng.dev.looker.com/embed/dashboards/321?hide_title=true&theme=' + lookerTheme
+}
---
+++ pbl.js
@@ -0,0 +1,86 @@
+var header = document.getElementById('header');
+header.textContent = globalConfig.headerText;
+
+title = document.getElementById('title')
+title.textContent = globalConfig.sidebarTitle
+
+// navbarImage = document.getElementById('navbar-logo')
+// navbarImage.setAttribute('src', globalConfig.logo)
+
+navbarHome = document.getElementById('navbar-home')
+navbarHome.innerHTML += globalConfig.logoText
+
+navMenu = document.getElementById('nav-menu')
+for (let i = 0; i < globalConfig.navbarMenu.length; i++) {
+  let menu = globalConfig.navbarMenu[i]
+  let li  = document.createElement('li')
+  li.innerHTML = '<a href="#">' + menu.text + '</a>'
+
+  li.addEventListener('click', showStaticPage)
+
+  navMenu.appendChild(li)
+}
+
+
+// SIDEBAR
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+sidebarImage = document.getElementById('sidebar-image')
+sidebarImage.setAttribute('src', globalConfig.sidebarImage)
+
+sidebarLogo = document.getElementById('sidebar-logo')
+sidebarLogo.setAttribute('src', globalConfig.logo)
+
+sidebarTitle = document.getElementById('sidebar-title')
+sidebarTitle.textContent = globalConfig.title
+
+slideOutMenu = document.getElementById('slide-out')
+for (let i = 0; i < globalConfig.sidebarItems.length; i++) {
+  item = globalConfig.sidebarItems[i]
+  if (item.type == 'li') {
+    let li = document.createElement('li')
+    let a = document.createElement('a')
+
+    a.className = 'waves-effect'
+    a.setAttribute('href', '#!')
+    a.setAttribute('looker-link', item.text)
+    a.innerHTML = '<i class="material-icons">' + item.icon + '</i>' + item.text
+
+    a.addEventListener('click', changeDashboard)
+
+    li.appendChild(a)
+    slideOutMenu.appendChild(li)
+  } else if (item.type == 'subheader') {
+    let divider = document.createElement('li')
+    divider.innerHTML = '<div class="divider"></div>'
+    slideOutMenu.appendChild(divider)
+
+    let li = document.createElement('li')
+    li.innerHTML = '<a class="subheader">' + item.text + '</a>'
+    slideOutMenu.appendChild(li)
+  }
+}
+
+// SET UP DEFAULT DASHBOARD
+mainDashboard = document.getElementById('looker_dashboard')
+mainDashboard.setAttribute('src', globalConfig.mainDashboard)
+
+
+function changeDashboard(e) {
+  newDash = e.target.attributes['looker-link'].value
+  newDashURL = globalConfig.dashboards[newDash]
+
+  header.textContent = newDash
+  mainDashboard.setAttribute('src', newDashURL)
+  mainDashboard.style.display = 'block'
+}
+
+
+function showStaticPage(e) {
+  console.log(e.target.textContent)
+  mainDashboard.setAttribute('src', 'business_value.html')
+}
